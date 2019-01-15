dbc2f78ca37a8be86abdc41ae2c99cb395cb372c
吴沛聪 <1097727091@qq.com>
2019-01-15 23:13:20 +0800

    一个小bug

--- role.miner.js
+++ role.miner.js
@@ -16,6 +16,9 @@ var roleMiner={
                 if(base.length){
                     creep.memory.sendToId=base[0].id;
                 }
+                else{
+                    creep.memory.role='builder';
+                }
             }
             if(creep.memory.sendToId){
                 var sendTo=Game.getObjectById(creep.memory.sendToId);
@@ -26,7 +29,7 @@ var roleMiner={
                 else if(creep.transfer(sendTo,RESOURCE_ENERGY)==ERR_FULL){
                     creep.memory.failTry+=1;
                     if(creep.memory.failTry>=20){
-                        creep.memory.role='builder';
+                        creep.memory.sendToId=0;
                         creep.memory.failTry=0;
                     }
                 }

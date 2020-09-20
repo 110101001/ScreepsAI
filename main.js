68ecbd1c0b8322f41d42e58a4284d85cf02fce06
吴沛聪 <1097727091@qq.com>
2020-09-20 22:59:26 +0800

    Rewrite(3rd time)

--- main.js
+++ main.js
@@ -1,32 +1,3 @@
-var production = require('production');
-var init = require('init');
-var role = require('role');
-/*Three layer structure: 
-Strategy Layer:room&mining site expansion, war
-
-Plan Layer:construction planning, task arrangment
-production.js
-Unit Layer:execute orders, control units
-role*.js
-*/
-
-
 module.exports.loop = function () {
-    if (init.isInit()) {
-        init.init();
-    }
-    if (Memory.init == true) {
-        //main loop
-        //strategy layer
-
-        //plan layer 
-        for (var r in Game.rooms) {
-            production.run(Game.rooms[r]);
-        }
-        //unit layer
-        for (var c in Game.creeps) {
-            role.run(Game.creeps[c]);
-        }
-
-    }
+	// Your code goes here
 }
